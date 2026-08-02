9d7612f219cbc37b2e94ec1fed32dc1691998c33
Jyotirmayanandasahu <jyotrymayanandasahu@gmail.com>
2026-08-02 20:06:52 +0530

    fix: ignore self-signed cert validation in development mode for SSL proxying

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
